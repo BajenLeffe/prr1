0cc8e8ab1c0a7bab8880d505b6bd59dc384d4940
sonicExe124 <alexandros.muller07@gmail.com>
2024-08-26 14:32:50 +0200

    upp1

---
+++ module0.py
@@ -0,0 +1,17 @@
+#välkommen till python
+
+"""hello everyone
+hej hej hej nej nej nej"""
+
+print("hello world")
+
+#ctrl + s för att spara
+#ctrl + f5 för att köra
+
+name = "ale"
+print(name)
+
+my_age = 17
+print(my_age)
+
+print(name + " is " + str(my_age) + " years old ")
---
+++ module1.py
@@ -0,0 +1,22 @@
+print("hello world!")
+
+print("""
+      nti
+        nti
+            nti
+                nti
+                    nti
+                nti
+            nti
+        nti
+            nti
+                nti
+                    nti""") #block comment
+
+print("=" * 90)
+
+name = input("write name:") #input is always standard text string
+
+#input tar in data från användaren av programmet och den sparat det i en variabel som är en sträng
+
+print("välkommen \""+ name + "\"") #namn output
