816fb81ba48866d56c14e4afcad2ca5676dc1cc1
sonicExe124 <alexandros.muller07@gmail.com>
2024-12-04 18:40:04 +0100

    module 7 added

---
+++ module7.py
@@ -0,0 +1,23 @@
+import os
+os.system('cls')
+
+cpu = [
+         {
+            "make":"AMD ryzen" ,
+            "class": 7,
+            "model":"5700X3D",
+            "clock": "4.1ghz"
+        },
+         {
+            "make":"AMD ryzen" ,
+            "class": 5,
+            "model":"5600",
+            "clock": "4.4ghz"
+        },
+         {
+            "make":"AMD ryzen", 
+            "class": 9,
+            "model":"5900XT",
+            "clock": "4.8ghz"
+        }
+]
