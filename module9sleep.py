343193ff790339c4179d40c9ba0bec6d534dae43
sonicExe124 <alexandros.muller07@gmail.com>
2025-03-19 12:16:50 +0100

    modul 9 nästan klar

--- module9sleep.py
+++ module9sleep.py
@@ -1,15 +1,14 @@
-
 import os
 from random import randint
 from colors import bcolors
 from time import sleep
-
+from msvcrt import getwch
 
 while True:
     os.system("cls")
     print("buffering")
     for i in range(randint(1, 5)):
-        for period in range(1,5):
+        for period in range(1, 5):
             sleep(0.3)
             os.system("cls")
             print("buffering", "." * period)
@@ -17,18 +16,29 @@ while True:
 
     os.system('cls')
     print("=" * 90)
-    print(bcolors.PURPLE+"Welcome to the game of Guess the number (1-100)!" "\n" "You get 7 tries! Go ahead!" "\n" "wanna quit? press 0"+bcolors.DEFAULT) #bcolors är en hjälpklass för att lägga färger på text i programmet
+    print(bcolors.PURPLE+"Welcome to the game of Guess the number (1-100)!" "\n" "You get 7 tries! Go ahead!" "\n" "wanna quit? press 0"+bcolors.DEFAULT)
 
     tries = 1
-    number = randint(1,100) #väljer ett random tal mellan 1-100
+    number = randint(1, 100)
 
     while tries <= 7:
         print("tries: ", tries)
 
         try:
-            guess = int(input("Put your guess here!: ")) #här gissar man
+            guess = ""
+            while not guess.isdigit():
+                char = getwch()
+                if char == "\r":  # Enter key pressed
+                    break
+                elif char.isdigit():
+                    guess += char
+                elif char == "\x1b":  # Escape key to quit
+                    guess = "0"
+                    break
+            
+            guess = int(guess)
         except:
-            print(bcolors.RED+"You sure that you put in a number there pal?"+bcolors.DEFAULT) #finns i fall någon skriver en bokstav
+            print(bcolors.RED+"You sure that you put in a number there pal?"+bcolors.DEFAULT)
             continue
 
         if guess == 0:
@@ -36,40 +46,32 @@ while True:
             break
         
         elif guess > number:
-            print(bcolors.CYAN+"Your guess is larger than the real number!"+bcolors.DEFAULT) #om man gissar för högt
+            print(bcolors.CYAN+"Your guess is larger than the real number!"+bcolors.DEFAULT)
 
         elif guess < number:
-            print(bcolors.CYAN+"your guess is too low!"+bcolors.DEFAULT) #om man gissar för lågt
+            print(bcolors.CYAN+"your guess is too low!"+bcolors.DEFAULT)
 
         elif guess == number:
-            print(bcolors.GREEN+"YAY YOU GOT IT!"+bcolors.DEFAULT) #om man får rätt
+            print(bcolors.GREEN+"YAY YOU GOT IT!"+bcolors.DEFAULT)
             break
         
-        tries += 1 #tickar på variabeln med 1 så den bryts efter 5 försök
+        tries += 1
 
     if tries == 8:
-        yesno = str(input(bcolors.BLUE+"sorry! you lost! try again? (Y/N): "+bcolors.DEFAULT)).upper() #finns om mer än 7 försök har gjorts
+        print(bcolors.BLUE+"sorry! you lost! try again? (Y/N): "+bcolors.DEFAULT, end="", flush=True)
+        yesno = getwch().upper()
+    else:
+        print(bcolors.GREEN+"\n" "yay! you finished the game! try again? (Y/N): "+bcolors.DEFAULT, end="", flush=True)
+        yesno = getwch().upper()
 
-    else:    
-        yesno = str(input(bcolors.GREEN+"\n" "yay! you finished the game! try again? (Y/N): "+bcolors.DEFAULT)).upper() #om man vinner
-
-    if yesno in ["NO","N"]:
-        print(bcolors.BOLD+"See you some other time!"+bcolors.DEFAULT) #hejdå meddelande om man vill inte fortsätta
+    if yesno in ["N"]:
+        print(bcolors.BOLD+"See you some other time!"+bcolors.DEFAULT)
         os.system("cls")
         print("exiting")
         for i in range(randint(1, 5)):
-            for period in range(1,5):
+            for period in range(1, 5):
                 sleep(0.3)
                 os.system("cls")
                 print("exiting", "." * period)
         os.system("cls")
         break
-    
-    else:
-        continue
-
-        
-        
-
-
-
