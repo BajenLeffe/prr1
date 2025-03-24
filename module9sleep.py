5c9039c05909a1bf8d3082e07424bf70b4bf2ed6
sonicExe124 <alexandros.muller07@gmail.com>
2025-03-24 14:45:01 +0100

    modul 9 nästan klar

--- module9sleep.py
+++ module9sleep.py
@@ -11,67 +11,61 @@ while True:
         for period in range(1, 5):
             sleep(0.3)
             os.system("cls")
-            print("buffering", "." * period)
+            print("buffering" + "." * period)
     os.system("cls")
 
-    os.system('cls')
     print("=" * 90)
-    print(bcolors.PURPLE+"Welcome to the game of Guess the number (1-100)!" "\n" "You get 7 tries! Go ahead!" "\n" "wanna quit? press 0"+bcolors.DEFAULT)
+    print(bcolors.PURPLE + "Welcome to the game of Guess the number (1-100)!" "\n" "You get 7 tries! Go ahead!" "\n" "Wanna quit? Press 0" + bcolors.DEFAULT)
 
     tries = 1
     number = randint(1, 100)
 
     while tries <= 7:
-        print("tries: ", tries)
+        print("Tries:", tries)
 
         try:
             guess = ""
             while not guess.isdigit():
                 char = getwch()
-                if char == "\r":  # Enter key pressed
-                    break
-                elif char.isdigit():
+                if char.isdigit():
                     guess += char
-                elif char == "\x1b":  # Escape key to quit
+                elif char == "\x1b":  #escape för att avsluta
                     guess = "0"
                     break
             
             guess = int(guess)
         except:
-            print(bcolors.RED+"You sure that you put in a number there pal?"+bcolors.DEFAULT)
+            print(bcolors.RED + "You sure that you put in a number there, pal?" + bcolors.DEFAULT)
             continue
 
         if guess == 0:
-            print("calling it quits?")
+            print("Calling it quits?")
             break
-        
         elif guess > number:
-            print(bcolors.CYAN+"Your guess is larger than the real number!"+bcolors.DEFAULT)
-
+            print(bcolors.CYAN + "Your guess is larger than the real number!" + bcolors.DEFAULT)
         elif guess < number:
-            print(bcolors.CYAN+"your guess is too low!"+bcolors.DEFAULT)
-
-        elif guess == number:
-            print(bcolors.GREEN+"YAY YOU GOT IT!"+bcolors.DEFAULT)
+            print(bcolors.CYAN + "Your guess is too low!" + bcolors.DEFAULT)
+        else:
+            print(bcolors.GREEN + "YAY YOU GOT IT!" + bcolors.DEFAULT)
             break
-        
+
         tries += 1
 
     if tries == 8:
-        print(bcolors.BLUE+"sorry! you lost! try again? (Y/N): "+bcolors.DEFAULT, end="", flush=True)
-        yesno = getwch().upper()
+        print(bcolors.BLUE + "Sorry! You lost! Try again? (Y/N): " + bcolors.DEFAULT)
     else:
-        print(bcolors.GREEN+"\n" "yay! you finished the game! try again? (Y/N): "+bcolors.DEFAULT, end="", flush=True)
-        yesno = getwch().upper()
+        print(bcolors.GREEN + "\nYay! You finished the game! Try again? (Y/N): " + bcolors.DEFAULT)
+    
+    yesno = getwch().upper()
 
-    if yesno in ["N"]:
-        print(bcolors.BOLD+"See you some other time!"+bcolors.DEFAULT)
+    if yesno == "N":
+        print(bcolors.BOLD + "See you some other time!" + bcolors.DEFAULT)
         os.system("cls")
-        print("exiting")
+        print("Exiting")
         for i in range(randint(1, 5)):
             for period in range(1, 5):
                 sleep(0.3)
                 os.system("cls")
-                print("exiting", "." * period)
+                print("Exiting" + "." * period)
         os.system("cls")
         break
