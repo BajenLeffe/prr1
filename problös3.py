db4d785a460cb58271485b904508560d7596e518
sonicExe124 <alexandros.muller07@gmail.com>
2025-04-09 12:11:09 +0200

    problemlösning klar

---
+++ problös3.py
@@ -0,0 +1,14 @@
+from time import sleep
+import os
+os.system('cls')
+
+timer = 5  
+
+print("Timer started:")
+for i in range(timer, 0, -1):
+    print(i)
+    sleep(1)
+    os.system('cls')
+
+
+print("FUCK OFF")
